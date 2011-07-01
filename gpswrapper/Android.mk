239688b3b420c2a1e9e90fe96de36dc8c9174151
Kolja Dummann <k.dummann@gmail.com>
2011-07-02 00:20:20 +0200

    complete rename

--- gpswrapper/Android.mk
+++ gpswrapper/Android.mk
@@ -1,11 +1,9 @@
-ifeq ($(BOARD_USES_GPSWRAPPER),true)
-
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_MODULE := gps.$(TARGET_BOOTLOADER_BOARD_NAME)
+LOCAL_MODULE := gps.smdkv310
 
 LOCAL_SHARED_LIBRARIES:= \
 	liblog \
@@ -21,5 +19,3 @@ LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 
 include $(BUILD_SHARED_LIBRARY)
-
-endif
